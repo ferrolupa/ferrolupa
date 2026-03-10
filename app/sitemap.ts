ba5b29e9723482555a42831542a2c27f78325bdd
Ferrolupa <ferrolupa.descargas@gmail.com>
2026-03-10 18:34:18 -0300

    corrijo dominio sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,15 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ferrolupa.com',
+      url: 'https://ferrolupa.com.ar',
       lastModified: new Date(),
     },
     {
-      url: 'https://ferrolupa.com/historias',
+      url: 'https://ferrolupa.com.ar/historias',
       lastModified: new Date(),
     },
     {
-      url: 'https://ferrolupa.com/descargas',
+      url: 'https://ferrolupa.com.ar/descargas',
       lastModified: new Date(),
     },
   ]
